f21c2256e8342e324aba5500b7324c6a4dbcb049
rramisa25-debug <rramisa25@gmail.com>
2026-05-10 17:09:08 +0600

    start.py

--- start.py
+++ start.py
@@ -1,11 +1,15 @@
 import subprocess
 import sys
+import os
 
-p1 = subprocess.Popen([sys.executable, "smm_bot_Final.py"])
-p2 = subprocess.Popen([sys.executable, "Claw_VIP_Final.py"])
+print("📁 Files in directory:", os.listdir('.'))
 
-print("SMM Bot চালু")
-print("Claw VIP Bot চালু")
+if name == "main":
+    p1 = subprocess.Popen([sys.executable, "smm_bot_Final.py"])
+    p2 = subprocess.Popen([sys.executable, "Claw_VIP_Final.py"])
 
-p1.wait()
-p2.wait()
+    print("✅ SMM Bot চালু!")
+    print("✅ Claw VIP Bot চালু!")
+
+    p1.wait()
+    p2.wait()
