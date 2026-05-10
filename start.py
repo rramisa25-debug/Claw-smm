6f7f4562acadaf38c9c761ce2ee99a2a98b0766f
rramisa25-debug <rramisa25@gmail.com>
2026-05-10 16:40:18 +0600

    start.py

--- start.py
+++ start.py
@@ -4,8 +4,8 @@ import sys
 p1 = subprocess.Popen([sys.executable, "smm_bot_Final.py"])
 p2 = subprocess.Popen([sys.executable, "Claw_VIP_Final.py"])
 
-print("✅ SMM Bot চালু!")
-print("✅ Claw VIP Bot চালু!")
+print("SMM Bot চালু")
+print("Claw VIP Bot চালু")
 
 p1.wait()
 p2.wait()
