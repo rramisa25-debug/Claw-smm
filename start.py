ac5f6dec13c33921b9b9744da52c796cc3641077
rramisa25-debug <rramisa25@gmail.com>
2026-05-10 17:30:19 +0600

    start.py

--- start.py
+++ start.py
@@ -2,14 +2,13 @@ import subprocess
 import sys
 import os
 
-print("📁 Files in directory:", os.listdir('.'))
+print("Files:", os.listdir('.'))
 
-if __name__== "__main__":
-    p1 = subprocess.Popen([sys.executable, "smm_bot_Final.py"])
-    p2 = subprocess.Popen([sys.executable, "Claw_VIP_Final.py"])
+p1 = subprocess.Popen([sys.executable, "smm_bot_Final.py"])
+p2 = subprocess.Popen([sys.executable, "Claw_VIP_Final.py"])
 
-    print("✅ SMM Bot চালু!")
-    print("✅ Claw VIP Bot চালু!")
+print("SMM Bot chalu!")
+print("Claw VIP Bot chalu!")
 
-    p1.wait()
-    p2.wait()
+p1.wait()
+p2.wait()
